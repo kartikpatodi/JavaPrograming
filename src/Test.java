8179b2b8e314245a318d39fc841508faaf4fed9f
Kartik Patodi <patodi.kartik@gmail.com>
2017-06-22 21:58:58 +0530

    Test.java

--- src/Test.java
+++ src/Test.java
@@ -2,59 +2,13 @@
  * Created by Kartik Patodi on 12-03-2017.
  **/
 
-import java.text.DecimalFormat;
-import java.util.*;
-
-class MyQueue<T>{
-    Stack<T> stack = new Stack<>();
-    Stack<T> queue = new Stack<>();
-
-
-    void enqueue(T val)
-    {
-        stack.push(val);
-    }
-
-    void dequeue()
-    {
-        if(queue.isEmpty()){
-            while(!stack.isEmpty()){
-                queue.push(stack.pop());
-            }
-        }
-        queue.pop();
-    }
-
-    T peek(){
-        if(queue.isEmpty()){
-            while(!stack.isEmpty()){
-                queue.push(stack.pop());
-            }
-        }
-        return queue.peek();
-    }
-
-}
-
 public class Test
 {
-    public static void main(String[] args) {
-        MyQueue<Integer> queue = new MyQueue<Integer>();
-        DecimalFormat format = new DecimalFormat("#.##");
-        Scanner scan = new Scanner(System.in);
-
-        int n = scan.nextInt();
-
-        for (int i = 0; i < n; i++) {
-            int operation = scan.nextInt();
-            if (operation == 1) { // enqueue
-                queue.enqueue(scan.nextInt());
-            } else if (operation == 2) { // dequeue
-                queue.dequeue();
-            } else if (operation == 3) { // print/peek
-                System.out.println(queue.peek());
-            }
-        }
-        scan.close();
+    public static void main(String[] args)
+    {
+//        int s = 'a'-96;
+//        char c = (char) 96;
+        String s = "@".toLowerCase();
+        System.out.println(s);
     }
 }
